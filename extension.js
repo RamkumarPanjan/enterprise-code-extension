fc875625b2a09dad372b643b3bf0b9acf84555d2
ashishsinghaus <ashish.singh.aus@gmail.com>
2023-07-28 14:27:51 +0530

    update

--- extension.js
+++ extension.js
@@ -36,13 +36,24 @@ vscode.workspace.onDidChangeTextDocument(event => {
 		var text = editor.document.getText();
 		var url='https://ashishsinghaus-super-trout-rxv66xj97q73pg4p-5000.preview.app.github.dev?lang=' + lang + '&hint= ' + text
 		axios.get(url).then(resp => {
-		editor.edit(editBuilder => {
-			editBuilder.insert(editor.selection.active, '\n' + resp.data);
-		});
+			codeComplete(editor, resp.data)
 		});
 
 	}
 });
+
+function codeComplete(editor, text) {
+    let index = 0;
+    const intervalId = setInterval(() => {
+        editor.edit((editBuilder) => {
+            editBuilder.insert(editor.selection.active, text[index]);
+        });
+        index++;
+        if (index >= text.length) {
+            clearInterval(intervalId);
+        }
+    }, 100);
+}
 
 function activate(context) {
 
@@ -53,7 +64,7 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('enterprise-code-extension.ecc', function () {
+	let disposable = vscode.commands.registerCommand('enterprise-code-extension.enterprise-code-companion', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
